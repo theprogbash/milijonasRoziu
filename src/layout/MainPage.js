fc2e98d15c6e74edd9ab97b23b186e71606b695c
Kamran Mmmdv <amorfrap@gmail.com>
2020-01-12 02:42:47 +0400

    Main page (static) completed

--- src/layout/MainPage.js
+++ src/layout/MainPage.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './MainPage.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import { faChevronLeft, faChevronRight, faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 
 class MainPage extends Component{
     render(){
@@ -25,7 +25,7 @@ class MainPage extends Component{
                         </header>
                         <nav className="Products-nav">
                             <div className="Left-nav">
-                                <h4>New Products</h4>
+                                <h5>New Products</h5>
                                 <div className="Header-wrapper"></div>
                             </div>
                             <div className="Right-nav">
@@ -119,6 +119,204 @@ class MainPage extends Component{
                                         </div>
                                     </div>
                                 </div>
+                                <div className="col-lg-3 col-md-6 col-sm12">
+                                    <div className="Product-card">
+                                        <div className="Product-img-container">
+                                            <div className="New-badge">New</div>
+                                            <img src={require("../assets/img/flower1.jpg")} alt="Cherry Lady"/>
+                                            <div className="Product-card-overlay">
+                                                <button className="Like-btn"><img src={require("../assets/img/heart.png")} alt="Love It"/></button>
+                                                <button className="Add-btn"><img src={require("../assets/img/bucket-black.png")} alt="Add to basket"/></button>
+                                            </div>
+                                        </div>
+                                        <div className="Product-details">
+                                            <div className="Product-name-container">
+                                                <h5 className="text-center">Cherry Lady</h5>
+                                            </div>
+                                            <div className="Product-buy-ops">
+                                                <span className="Product-price">250.00 AZN</span>
+                                                <button><img src={require("../assets/img/bag.png")} alt="Add to basket"/></button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-lg-3 col-md-6 col-sm12">
+                                    <div className="Product-card">
+                                        <div className="Product-img-container">
+                                            <div className="New-badge">New</div>
+                                            <img src={require("../assets/img/flower1.jpg")} alt="Cherry Lady"/>
+                                            <div className="Product-card-overlay">
+                                                <button className="Like-btn"><img src={require("../assets/img/heart.png")} alt="Love It"/></button>
+                                                <button className="Add-btn"><img src={require("../assets/img/bucket-black.png")} alt="Add to basket"/></button>
+                                            </div>
+                                        </div>
+                                        <div className="Product-details">
+                                            <div className="Product-name-container">
+                                                <h5 className="text-center">Cherry Lady</h5>
+                                            </div>
+                                            <div className="Product-buy-ops">
+                                                <span className="Product-price">250.00 AZN</span>
+                                                <button><img src={require("../assets/img/bag.png")} alt="Add to basket"/></button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-lg-3 col-md-6 col-sm12">
+                                    <div className="Product-card">
+                                        <div className="Product-img-container">
+                                            <div className="New-badge">New</div>
+                                            <img src={require("../assets/img/flower1.jpg")} alt="Cherry Lady"/>
+                                            <div className="Product-card-overlay">
+                                                <button className="Like-btn"><img src={require("../assets/img/heart.png")} alt="Love It"/></button>
+                                                <button className="Add-btn"><img src={require("../assets/img/bucket-black.png")} alt="Add to basket"/></button>
+                                            </div>
+                                        </div>
+                                        <div className="Product-details">
+                                            <div className="Product-name-container">
+                                                <h5 className="text-center">Cherry Lady</h5>
+                                            </div>
+                                            <div className="Product-buy-ops">
+                                                <span className="Product-price">250.00 AZN</span>
+                                                <button><img src={require("../assets/img/bag.png")} alt="Add to basket"/></button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-lg-3 col-md-6 col-sm12">
+                                    <div className="Product-card">
+                                        <div className="Product-img-container">
+                                            <div className="New-badge">New</div>
+                                            <img src={require("../assets/img/flower1.jpg")} alt="Cherry Lady"/>
+                                            <div className="Product-card-overlay">
+                                                <button className="Like-btn"><img src={require("../assets/img/heart.png")} alt="Love It"/></button>
+                                                <button className="Add-btn"><img src={require("../assets/img/bucket-black.png")} alt="Add to basket"/></button>
+                                            </div>
+                                        </div>
+                                        <div className="Product-details">
+                                            <div className="Product-name-container">
+                                                <h5 className="text-center">Cherry Lady</h5>
+                                            </div>
+                                            <div className="Product-buy-ops">
+                                                <span className="Product-price">250.00 AZN</span>
+                                                <button><img src={require("../assets/img/bag.png")} alt="Add to basket"/></button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="Products-bottom">
+                            <div>
+                                <section>
+                                    <h5>Flowers</h5>
+                                    <p className="Products-bottom-description">The most striking events in life, never do without flowers, compliments and gifts! We present to you our new collection of original presents and stunning gift ideas that will definitely appeal to everyone: mom, grandmother, wife, daughter, sister, girlfriend and beloved half! We guarantee a sea of emotions and delight!</p>
+                                    <button className="Read-more">Read More</button>
+                                </section>
+                            </div>
+                            <div>
+                                <div className="Product-bottom-img1"></div>
+                            </div>
+                            <div>
+                                <div className="Product-bottom-img2"></div>
+                            </div>
+                            <div>
+                                <section>
+                                    <h5>Gifts</h5>
+                                    <p className="Products-bottom-description">Our gifts are made of stunning and original ideas. Gifts are for you and your family's closest family members: fathers, mothers, grandfathers, grandmothers, sisters, brothers, and your beloved. Our professional team can help you choose gifts.</p>
+                                    <button className="Read-more">Read More</button>
+                                </section>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="Best-sellers">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-lg-6 col-md-12 col-sm-12">
+                                <nav className="Products-nav">
+                                    <div className="Left-nav">
+                                        <h5>Best Seller</h5>
+                                        <div className="Header-wrapper"></div>
+                                    </div>
+                                    <div className="Right-nav">
+                                        <span><FontAwesomeIcon icon={faChevronLeft}/></span>
+                                        <span><FontAwesomeIcon icon={faChevronRight}/></span>
+                                    </div>
+                                </nav>
+                                <div className="row">
+                                    <div className="col-lg-6 col-md-6 col-sm12">
+                                    <div className="Product-card">
+                                        <div className="Product-img-container">
+                                            <div className="New-badge">New</div>
+                                            <img src={require("../assets/img/flower5.jpg")} alt="Cherry Lady"/>
+                                            <div className="Product-card-overlay">
+                                                <button className="Like-btn"><img src={require("../assets/img/heart.png")} alt="Love It"/></button>
+                                                <button className="Add-btn"><img src={require("../assets/img/bucket-black.png")} alt="Add to basket"/></button>
+                                            </div>
+                                        </div>
+                                        <div className="Product-details">
+                                            <div className="Product-name-container">
+                                                <h5 className="text-center">Giselle</h5>
+                                            </div>
+                                            <div className="Product-buy-ops">
+                                                <span className="Product-price">408.00 AZN</span>
+                                                <button><img src={require("../assets/img/bag.png")} alt="Add to basket"/></button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                    <div className="col-lg-6 col-md-6 col-sm12">
+                                    <div className="Product-card">
+                                        <div className="Product-img-container">
+                                            <div className="New-badge">New</div>
+                                            <img src={require("../assets/img/flower1.jpg")} alt="Cherry Lady"/>
+                                            <div className="Product-card-overlay">
+                                                <button className="Like-btn"><img src={require("../assets/img/heart.png")} alt="Love It"/></button>
+                                                <button className="Add-btn"><img src={require("../assets/img/bucket-black.png")} alt="Add to basket"/></button>
+                                            </div>
+                                        </div>
+                                        <div className="Product-details">
+                                            <div className="Product-name-container">
+                                                <h5 className="text-center">Royal Composition</h5>
+                                            </div>
+                                            <div className="Product-buy-ops">
+                                                <span className="Product-price">750.00 AZN</span>
+                                                <button><img src={require("../assets/img/bag.png")} alt="Add to basket"/></button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                </div>
+                            </div>
+                            <div className="col-lg-6 col-md-12 col-sm-12">
+                                <div className="Best-seller"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="Follow-section">
+                    <div className="Follow-section-header text-center">
+                        <p>Follow us on Instagram</p>
+                        <p>@milijonasroziu</p>
+                    </div>
+                    <div className="Follow-section-cover">
+                        <img src={require("../assets/img/cover.png")} alt="Image not found :/"/>
+                    </div>
+                </div>
+                <div className="Subscribe-section">
+                    <div className="Subscribe-box">
+                        <div className="Left-subscribe-box">
+                            <h5>Subscribe to newsletters</h5>
+                            <div className="Subscribe-box-inputs">
+                                <input type="email" placeholder="Email"/>
+                                <button className="Email-sub-button">
+                                    <FontAwesomeIcon icon={faPaperPlane} />
+                                </button>
+                            </div>
+                        </div>
+                        <div className="Right-subscribe-box">
+                            <div className="Deliver-img-container">
+                                <img src={require("../assets/img/deliver.png")} alt=""/>
                             </div>
                         </div>
                     </div>
