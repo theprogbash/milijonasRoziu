8b5422d95f6a16b224624e973bdd4dec5a238045
Kamran Mmmdv <amorfrap@gmail.com>
2020-02-11 22:21:04 +0400

    Added all transitions to main page

--- src/layout/MainPage.js
+++ src/layout/MainPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Radium, { StyleRoot } from 'radium';
 import './MainPage.css';
 import '../common/ProductCard';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -6,13 +7,29 @@ import { faChevronLeft, faChevronRight, faPaperPlane } from "@fortawesome/free-s
 import ProductCard from '../common/ProductCard';
 import PageName from '../common/PageName';
 
-class MainPage extends Component{
-    render(){
-        return(
-            <div className="Main-page">
+import { slideInLeft, slideInRight, flipInX } from 'react-animations';
+const styles = {
+    slideInLeft: {
+        animation: 'x 1s',
+        animationName: Radium.keyframes(slideInLeft, 'slideInLeft')
+    },
+    slideInRight: {
+        animation: 'x 1s',
+        animationName: Radium.keyframes(slideInRight, 'slideInRight')
+    },
+    flipInX: {
+        animation: 'x 1s',
+        animationName: Radium.keyframes(flipInX, 'flipInX')
+    }
+}
+
+class MainPage extends Component {
+    render() {
+        return (
+            <StyleRoot className="Main-page">
                 <header className="Main-page-header">
                     <div className="container">
-                        <div className="Slider-box">
+                        <div className="Slider-box" style={styles.slideInLeft}>
                             <div className="Slider-box-content">
                                 <h4>Flowers</h4>
                                 <h4>Choose from the most beatiful flowers</h4>
@@ -32,35 +49,43 @@ class MainPage extends Component{
                                 <div className="Header-wrapper"></div>
                             </div>
                             <div className="Right-nav">
-                                <span><FontAwesomeIcon icon={faChevronLeft}/></span>
-                                <span><FontAwesomeIcon icon={faChevronRight}/></span>
+                                <span><FontAwesomeIcon icon={faChevronLeft} /></span>
+                                <span><FontAwesomeIcon icon={faChevronRight} /></span>
                             </div>
                         </nav>
                         <div className="Products-content-slider">
                             <div className="row">
-                                <div className="col-lg-3 col-md-6 col-6">
-                                    <ProductCard ProductName={"Cherry Lady"} ProductPrice={"250.00"} ProductImgSrc={require('../assets/img/flower1.jpg')}/>
+                                <div className="col-lg-6 col-md-12 col-12" style={styles.slideInLeft}>
+                                    <div className="row">
+                                        <div className="col-lg-6 col-md-6 col-6">
+                                            <ProductCard ProductName={"Cherry Lady"} ProductPrice={"250.00"} ProductImgSrc={require('../assets/img/flower1.jpg')} />
+                                        </div>
+                                        <div className="col-lg-6 col-md-6 col-6">
+                                            <ProductCard ProductName={"Constellation"} ProductPrice={"400.00"} ProductImgSrc={require('../assets/img/flower2.jpg')} />
+                                        </div>
+                                        <div className="col-lg-6 col-md-6 col-6">
+                                            <ProductCard ProductName={"August Birth"} ProductPrice={"260.00"} ProductImgSrc={require('../assets/img/flower3.jpg')} />
+                                        </div>
+                                        <div className="col-lg-6 col-md-6 col-6">
+                                            <ProductCard ProductName={"Cherry Lady"} ProductPrice={"250.00"} ProductImgSrc={require('../assets/img/flower1.jpg')} />
+                                        </div>
+                                    </div>
                                 </div>
-                                <div className="col-lg-3 col-md-6 col-6">
-                                    <ProductCard ProductName={"Constellation"} ProductPrice={"400.00"} ProductImgSrc={require('../assets/img/flower2.jpg')}/>   
-                                </div>
-                                <div className="col-lg-3 col-md-6 col-6">
-                                    <ProductCard ProductName={"August Birth"} ProductPrice={"260.00"} ProductImgSrc={require('../assets/img/flower3.jpg')}/>
-                                </div>
-                                <div className="col-lg-3 col-md-6 col-6">
-                                    <ProductCard ProductName={"Cherry Lady"} ProductPrice={"250.00"} ProductImgSrc={require('../assets/img/flower1.jpg')}/>
-                                </div>
-                                <div className="col-lg-3 col-md-6 col-6">
-                                    <ProductCard ProductName={"Constellation"} ProductPrice={"400.00"} ProductImgSrc={require('../assets/img/flower2.jpg')}/>   
-                                </div>
-                                <div className="col-lg-3 col-md-6 col-6">
-                                    <ProductCard ProductName={"August Birth"} ProductPrice={"410.00"} ProductImgSrc={require('../assets/img/flower3.jpg')}/>
-                                </div>
-                                <div className="col-lg-3 col-md-6 col-6">
-                                    <ProductCard ProductName={"Cherry Lady"} ProductPrice={"250.00"} ProductImgSrc={require('../assets/img/flower1.jpg')}/>
-                                </div>
-                                <div className="col-lg-3 col-md-6 col-6">
-                                    <ProductCard ProductName={"Constellation"} ProductPrice={"400.00"} ProductImgSrc={require('../assets/img/flower2.jpg')}/>   
+                                <div className="col-lg-6 col-md-12 col-12" style={styles.slideInRight}>
+                                    <div className="row">
+                                        <div className="col-lg-6 col-md-6 col-6">
+                                            <ProductCard ProductName={"Constellation"} ProductPrice={"400.00"} ProductImgSrc={require('../assets/img/flower2.jpg')} />
+                                        </div>
+                                        <div className="col-lg-6 col-md-6 col-6">
+                                            <ProductCard ProductName={"August Birth"} ProductPrice={"410.00"} ProductImgSrc={require('../assets/img/flower3.jpg')} />
+                                        </div>
+                                        <div className="col-lg-6 col-md-6 col-6">
+                                            <ProductCard ProductName={"Cherry Lady"} ProductPrice={"250.00"} ProductImgSrc={require('../assets/img/flower1.jpg')} />
+                                        </div>
+                                        <div className="col-lg-6 col-md-6 col-6">
+                                            <ProductCard ProductName={"Constellation"} ProductPrice={"400.00"} ProductImgSrc={require('../assets/img/flower2.jpg')} />
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -73,10 +98,10 @@ class MainPage extends Component{
                                 </section>
                             </div>
                             <div>
-                                <div className="Product-bottom-img1"></div>
+                                <div className="Product-bottom-img1" style={styles.flipInX}></div>
                             </div>
                             <div>
-                                <div className="Product-bottom-img2"></div>
+                                <div className="Product-bottom-img2" style={styles.flipInX}></div>
                             </div>
                             <div>
                                 <section>
@@ -98,21 +123,21 @@ class MainPage extends Component{
                                         <div className="Header-wrapper"></div>
                                     </div>
                                     <div className="Right-nav">
-                                        <span><FontAwesomeIcon icon={faChevronLeft}/></span>
-                                        <span><FontAwesomeIcon icon={faChevronRight}/></span>
+                                        <span><FontAwesomeIcon icon={faChevronLeft} /></span>
+                                        <span><FontAwesomeIcon icon={faChevronRight} /></span>
                                     </div>
                                 </nav>
-                                <div className="row">
+                                <div className="row" style={styles.slideInLeft}>
                                     <div className="col-lg-6 col-md-6 col-6">
-                                        <ProductCard ProductImgSrc={require("../assets/img/flower5.jpg")} ProductName={"Giselle"} ProductPrice={"408.00"}/>
+                                        <ProductCard ProductImgSrc={require("../assets/img/flower5.jpg")} ProductName={"Giselle"} ProductPrice={"408.00"} />
                                     </div>
                                     <div className="col-lg-6 col-md-6 col-6">
-                                        <ProductCard ProductImgSrc={require("../assets/img/flower6.jpg")} ProductName={"Royal"} ProductPrice={"750.00"}/>   
+                                        <ProductCard ProductImgSrc={require("../assets/img/flower6.jpg")} ProductName={"Royal"} ProductPrice={"750.00"} />
                                     </div>
                                 </div>
                             </div>
                             <div className="col-lg-6 col-md-12 col-sm-12">
-                                <div className="Best-seller"></div>
+                                <div className="Best-seller" style={styles.slideInRight}></div>
                             </div>
                         </div>
                     </div>
@@ -123,7 +148,7 @@ class MainPage extends Component{
                         <p>@milijonasroziu</p>
                     </div>
                     <div className="Follow-section-cover">
-                        <img src={require("../assets/img/cover.png")} alt="Image not found :/"/>
+                        <img src={require("../assets/img/cover.png")} alt="Image not found :/" />
                     </div>
                 </div>
                 <div className="Subscribe-section">
@@ -131,7 +156,7 @@ class MainPage extends Component{
                         <div className="Left-subscribe-box">
                             <h5>Subscribe to newsletters</h5>
                             <div className="Subscribe-box-inputs">
-                                <input type="email" placeholder="Email"/>
+                                <input type="email" placeholder="Email" />
                                 <button className="Email-sub-button">
                                     <FontAwesomeIcon icon={faPaperPlane} />
                                 </button>
@@ -139,12 +164,12 @@ class MainPage extends Component{
                         </div>
                         <div className="Right-subscribe-box">
                             <div className="Deliver-img-container">
-                                <img src={require("../assets/img/deliver.png")} alt=""/>
+                                <img src={require("../assets/img/deliver.png")} alt="" />
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </StyleRoot>
         );
     }
 }
