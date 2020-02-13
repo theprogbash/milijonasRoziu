0e1af1db5c967fdd8632fb0bf447d0d3f21c307c
Kamran Mmmdv <amorfrap@gmail.com>
2020-02-13 21:05:55 +0400

    Created login-register page

--- src/layout/LoginReg.js
+++ src/layout/LoginReg.js
@@ -1,13 +1,65 @@
 import React, { Component } from 'react';
 import './LoginReg.css';
 import Breadcrumb from '../common/Breadcrumb';
+import $ from 'jquery';
+window.$ = $;
 
-class LoginReg extends Component{
-    render(){
-        return(
+class LoginReg extends Component {
+    render() {
+        function MinimizeLabel(){
+            $(this).closest(".Reg-fields").css("border-bottom", "2px solid #f99");
+            $(this).closest("label").css("top", "-15px").css("font-size", ".7em").css("transition", ".3s ease-out");
+        }
+        
+        return (
             <div>
                 <div className="container">
-                    <Breadcrumb currentPage="Login and register"/>
+                    <Breadcrumb currentPage="Login and register" />
+
+                    <div className="Loginreg-container">
+                        <div className="row">
+                            <div className="Login-section col-lg-6 col-md-12 col-12">
+                                <div className="custom-container">
+                                    <div>
+                                        <h5>Log in</h5>
+                                    </div>
+                                    <div className="Reg-fields">
+                                        <input type="email" onClick={MinimizeLabel}/>
+                                        <label>Email</label>
+                                    </div>
+                                    <div className="Reg-fields">
+                                        <input type="password" onClick={MinimizeLabel}/>
+                                        <label>Password</label>
+                                    </div>
+                                    <div className="float-right">
+                                        <button className="Login-btn">Log in</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="Register-section col-lg-6 col-md-12 col-12">
+                                <div className="custom-container">
+                                    <div>
+                                        <h5>Register</h5>
+                                    </div>
+                                    <div className="Reg-fields">
+                                        <input type="email" onClick={MinimizeLabel}/>
+                                        <label>Email</label>
+                                    </div>
+                                    <div className="Reg-fields">
+                                        <input type="password" onClick={MinimizeLabel}/>
+                                        <label>Password</label>
+                                    </div>
+                                    <div className="Reg-fields">
+                                        <input type="password" onClick={MinimizeLabel}/>
+                                        <label>Confirm Password</label>
+                                    </div>
+                                    <div className="float-right">
+                                        <button className="Login-btn">Register</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
